a3107055b64273ec8a1148548ef6602866d91729
Daniel Amesberger <daniel.amesberger@amescon.com>
2021-03-01 20:32:06 +0100

    switched motor numbering for motor3-6

--- dcmctrl.v
+++ dcmctrl.v
@@ -382,24 +382,24 @@ module top  (
 	output SLOT2_IO8, //MODE     (2)
 	output SLOT2_IO9,
 
-	output SLOT3_IO0, //PWM_A    (3)
-	output SLOT3_IO1, //PWM_B    (3)
-	output SLOT3_IO2, //PWM_C    (3)
-	output SLOT3_IO3, //PWM_D    (3)
-	output SLOT3_IO4, //RESET_AB (3)
-	output SLOT3_IO5, //RESET_CD (3)
-	input SLOT3_IO6, //FAULT    (3)
-	input  SLOT3_IO7, //OTW      (3)
-	output SLOT3_IO8, //MODE     (3)
-	output SLOT3_IO9,
+	output SLOT4_IO0, //PWM_A    (3)
+	output SLOT4_IO1, //PWM_B    (3)
+	output SLOT4_IO2, //PWM_C    (3)
+	output SLOT4_IO3, //PWM_D    (3)
+	output SLOT4_IO4, //RESET_AB (3)
+	output SLOT4_IO5, //RESET_CD (3)
+	input  SLOT4_IO6, //FAULT    (3)
+	input  SLOT4_IO7, //OTW      (3)
+	output SLOT4_IO8, //MODE     (3)
+	output SLOT4_IO9,
 
-	input  SLOT4_IO0, //HALL 1
-	input  SLOT4_IO1, //HALL 2
-	input  SLOT4_IO2, //HALL 3
-	input  SLOT4_IO3, //HALL 4
-	input  SLOT4_IO4, //HALL 5
-	input  SLOT4_IO5,  //HALL 6
-	input  SLOT4_IO6  //RESET
+	input  SLOT3_IO0, //HALL 1
+	input  SLOT3_IO1, //HALL 2
+	input  SLOT3_IO2, //HALL 3
+	input  SLOT3_IO3, //HALL 4
+	input  SLOT3_IO4, //HALL 5
+	input  SLOT3_IO5,  //HALL 6
+	input  SLOT3_IO6  //RESET
 	//output SLOT4_IO7,
 	//output SLOT4_IO8,
 	//output SLOT4_IO9
@@ -415,14 +415,15 @@ module top  (
 	wire  [7:0] motor_fault = 0;
 	wire  [7:0] motor_otw = 0;
 
-    //TODO - reset functionality needs to be implemented
 	localparam RESET = 1;
 	assign SLOT1_IO4 = RESET;
 	assign SLOT1_IO5 = RESET;
+
 	assign SLOT2_IO4 = RESET;
 	assign SLOT2_IO5 = RESET;
-	assign SLOT3_IO4 = RESET;
-	assign SLOT3_IO5 = RESET;
+
+	assign SLOT4_IO4 = RESET;
+	assign SLOT4_IO5 = RESET;
 
 	//SLOT 1
 	assign motor_left[1]  = SLOT1_IO0;
@@ -438,45 +439,47 @@ module top  (
 	assign SLOT1_IO8      = MODE;
 
 	//SLOT 2
-	assign motor_left[2]  = SLOT2_IO0;
-	assign motor_right[2] = SLOT2_IO1;
-	assign motor_left[3]  = SLOT2_IO2;
-	assign motor_right[3] = SLOT2_IO3;
-	//assign motor_reset[2] = !SLOT2_IO4;
-	//assign motor_reset[3] = !SLOT2_IO5;
-	assign motor_fault[2] = !SLOT2_IO6;
+	assign motor_left[3]  =  SLOT2_IO0;
+	assign motor_right[3] =  SLOT2_IO1;
+	assign motor_left[2]  =  SLOT2_IO2;
+	assign motor_right[2] =  SLOT2_IO3;
+	//assign motor_reset[3] = !SLOT2_IO4;
+	//assign motor_reset[2] = !SLOT2_IO5;
 	assign motor_fault[3] = !SLOT2_IO6;
-	assign motor_otw[2]   = !SLOT2_IO7;
+	assign motor_fault[2] = !SLOT2_IO6;
 	assign motor_otw[3]   = !SLOT2_IO7;
+	assign motor_otw[2]   = !SLOT2_IO7;
 	assign SLOT2_IO8      = MODE;
 
-	//SLOT 3
-	assign motor_left[4]  = SLOT3_IO0;
-	assign motor_right[4] = SLOT3_IO1;
-	assign motor_left[5]  = SLOT3_IO2;
-	assign motor_right[5] = SLOT3_IO3;
-	//assign motor_reset[4] = !SLOT3_IO4;
-	//assign motor_reset[5] = !SLOT3_IO5;
-	assign motor_fault[4] = !SLOT3_IO6;
-	assign motor_fault[5] = !SLOT3_IO6;
-	assign motor_otw[4]   = !SLOT3_IO7;
-	assign motor_otw[5]   = !SLOT3_IO7;
+	//SLOT 4
+	assign motor_left[5]  =  SLOT4_IO0;
+	assign motor_right[5] =  SLOT4_IO1;
+	assign motor_left[4]  =  SLOT4_IO2;
+	assign motor_right[4] =  SLOT4_IO3;
+	//assign motor_reset[5] = !SLOT4_IO4;
+	//assign motor_reset[4] = !SLOT4_IO5;
+	assign motor_fault[5] = !SLOT4_IO6;
+	assign motor_fault[4] = !SLOT4_IO6;
+	assign motor_otw[5]   = !SLOT4_IO7;
+	assign motor_otw[4]   = !SLOT4_IO7;
 	assign SLOT3_IO8      = MODE;
 
-	//SLOT 4
-	assign motor_pulse[0] = SLOT4_IO0;
-	assign motor_pulse[1] = SLOT4_IO1;
-	assign motor_pulse[2] = SLOT4_IO2;
-	assign motor_pulse[3] = SLOT4_IO3;
-	assign motor_pulse[4] = SLOT4_IO4;
-	assign motor_pulse[5] = SLOT4_IO5;
+	//SLOT 3
+	assign motor_pulse[0] = SLOT3_IO0;
+	assign motor_pulse[1] = SLOT3_IO1;
+	assign motor_pulse[2] = SLOT3_IO2;
+	assign motor_pulse[3] = SLOT3_IO3;
+	assign motor_pulse[4] = SLOT3_IO4;
+	assign motor_pulse[5] = SLOT3_IO5;
 
-	wire LED = LED1_1;
-	assign LED = LED1_2;
-	assign LED = SLOT1_IO4;
+    //LED
+	//localparam LED_STATUS = 1;
+	//wire LED = LED1_1;
+	//assign LED = LED1_2;
+	//assign LED = !LED_STATUS;
 
-//assign LED1_1 = SLOT1_IO4;
-//assign LED1_2 = SLOT1_IO4;
+assign LED1_1 = SLOT3_IO0;
+assign LED1_2 = SLOT3_IO0;
 
 // Powerup Reset Logic
 // generate a reset pulse on initial powerup
@@ -490,9 +493,6 @@ always @(posedge clk)
    end
 
 wire reset = pup_reset;
-//wire SLOT1_IO9 = reset;
-
-
 
 dcmctrl uut (
 	.clk(clk),
